2fb5530f08b82febd266c3d62e07c754dc123c53
ryoma <asagi.fuzichoco.0211@gmail.com>
2026-03-13 13:06:45 +0900

    headerあとリンク張りつけ

--- static/js/header_script.js
+++ static/js/header_script.js
@@ -1,8 +1,26 @@
 function to_mypage() {
-    location.href = "/dashboard.html";
+    location.href = "dashboard.html";
 }
 
 function logout() {
     localStorage.removeItem("username");
     location.href = "login.html";
 }
+
+const menuButton = document.getElementById("menu-button");
+const overlay = document.getElementById("overlay");
+const sidemenu = document.getElementById("sidemenu");
+
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("active");
+    overlay.classList.toggle("active");
+    sidemenu.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+    menuButton.classList.remove("active");
+    overlay.classList.remove("active");
+    sidemenu.classList.remove("active");
+});
+
+
